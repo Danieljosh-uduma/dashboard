030a591a1964472db41215d19f7fb0869fe8aac3
Daniel Josh <danieludumajosh@gmail.com>
2025-03-20 15:05:07 +0100

    ui completion

--- src/components/Main/Header/Header.tsx
+++ src/components/Main/Header/Header.tsx
@@ -1,6 +1,36 @@
+import CalenderImg from '/src/assets/images/calender.svg'
+import ProfilePicImg from '/src/assets/images/profile-pic.svg'
+import BoardImg from '/src/assets/images/board.svg'
 import './Header.css'
 
 
 export default function Header() {
-    return
+    return (
+        <header>
+            <div className="header">
+                <h2>Welcome back, Josh</h2>
+                <div className='info'>
+                    <div className='date'>
+                        <img src={CalenderImg} alt="" />
+                        <p>19 March 2025</p>
+                    </div>
+                    <img className='profile' src={ProfilePicImg} alt="" />
+                </div>
+            </div>
+
+            <div className="footer">
+                <div>
+                    <figure className='active-view'>
+                        <img src={BoardImg} alt="" />
+                        <figcaption>Board view</figcaption>
+                    </figure>
+                    <div className='add-view'>
+                        <span>+</span>
+                        <span>Add view</span>
+                    </div>
+                </div>
+                <button>New template</button>
+            </div>
+        </header>
+    )
 }
--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -5,9 +5,6 @@ import { useState } from 'react'
 import './Sidebar.css'
 
 export default function Sidebar() {
-    
-    
-
     return (
         <>
            <nav className='sidebar'>
